2ced7e864859caf0f45c9eac2860f6e35715ecf1
Hei Fung <heifung@Heis-MacBook-Pro.local>
2023-12-19 12:00:12 +0000

    Replaced the data with API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import './App.scss'
-import BeerCard from './components/BeerCard/BeerCard'
-import beers from './data/beers'
-function App() {
+import './App.scss';
+import BeerCard from './components/BeerCard/BeerCard';
+import { Beer } from './types/types';
+import { useEffect, useState } from 'react';
 
+function App() {
+  const [beers, setBeers] = useState<Beer[]>();
+
+  const getBeers = async () => {
+    const response = await fetch("https://api.punkapi.com/v2/beers");
+    const data: Beer[] = await response.json();
+    setBeers(data)
+  };
+
+  useEffect(() => {
+    getBeers();
+  }, []);
   return (
     <>
-    <BeerCard beers={beers}/>
+    {beers && <BeerCard beers={beers}/>}
     </>
   )
 }
