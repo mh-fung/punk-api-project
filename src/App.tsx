1d523548cffd91cecf1162469b0c58d5a4149612
Hei Fung <heifung@Heis-MacBook-Pro.local>
2023-12-27 11:43:09 +0000

    Add more requests for more data from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.scss';
 import ExploreBeers from './components/containers/ExploreBeers/ExploreBeers';
+// import endpoints from './data/endpoints';
 import { Beer } from './types/types';
 import { useEffect, useState } from 'react';
 
@@ -7,10 +8,29 @@ function App() {
   const [beers, setBeers] = useState<Beer[]>();
 
   const getBeers = async () => {
-    const response = await fetch("https://api.punkapi.com/v2/beers");
-    const data: Beer[] = await response.json();
-    setBeers(data)
+    const responsePage1 = await fetch("https://api.punkapi.com/v2/beers?page=1&per_page=80");
+    const dataPage1: Beer[] = await responsePage1.json();
+    const responsePage2 = await fetch("https://api.punkapi.com/v2/beers?page=2&per_page=80");
+    const dataPage2: Beer[] = await responsePage2.json();
+    const responsePage3 = await fetch("https://api.punkapi.com/v2/beers?page=3&per_page=80");
+    const dataPage3: Beer[] = await responsePage3.json();
+    const responsePage4 = await fetch("https://api.punkapi.com/v2/beers?page=4&per_page=80");
+    const dataPage4: Beer[] = await responsePage4.json();
+    const responsePage5 = await fetch("https://api.punkapi.com/v2/beers?page=5&per_page=80");
+    const dataPage5: Beer[] = await responsePage5.json();
+    const dataAll = dataPage1.concat(dataPage2, dataPage3, dataPage4, dataPage5)
+    setBeers(dataAll)
   };
+  //   let dataAll: Beer[]
+    
+  //   endpoints.forEach(async (endpoint) => {
+  //     const response = await fetch(endpoint);
+  //     const data: Beer[] = await response.json();
+  //     dataAll = dataAll.concat(data);
+  //   });
+
+  //   setBeers(dataAll)
+  // };
 
   useEffect(() => {
     getBeers();
---
+++ src/data/endpoints.ts
@@ -0,0 +1,9 @@
+const endpoints: string[] = [
+    "https://api.punkapi.com/v2/beers?page=1&per_page=80", 
+    "https://api.punkapi.com/v2/beers?page=2&per_page=80",
+    "https://api.punkapi.com/v2/beers?page=3&per_page=80", 
+    "https://api.punkapi.com/v2/beers?page=4&per_page=80", 
+    "https://api.punkapi.com/v2/beers?page=5&per_page=80", 
+];
+
+export default endpoints;
